ec48d59fc0bf3ba769a5a012345cdd4bc24b652f
Nguithu <nguithufrancis@gmail.com>
2021-07-26 06:20:37 +0300

    add styles

--- script.js
+++ script.js
@@ -16,3 +16,14 @@ $ (document) .ready(function(){
   
   
   });
+
+
+  $ (document) .ready(function(){
+    $("#shown").click(function(){
+      $("#put").toggle();
+    
+    
+    })
+    
+    
+    });

59e469907db550a68485dda32daf682571d7120b
Nguithu <nguithufrancis@gmail.com>
2021-07-26 05:25:29 +0300

    add script.js and styles improved

---
+++ script.js
@@ -0,0 +1,18 @@
+$ (document) .ready(function(){
+$("#menu-toggle").click(function(){
+  $("#text-hidden").toggle();
+
+
+})
+
+
+});
+$ (document) .ready(function(){
+  $("#hidden").click(function(){
+    $("#text").toggle();
+  
+  
+  })
+  
+  
+  });
